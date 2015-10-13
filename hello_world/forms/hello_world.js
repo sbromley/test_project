acd6c4530d49b92e86bc665f211a72bb546b4565
Scott Bromley <sb@qualitas.co.uk>
2015-10-13 14:20:09 +0100

    1

---
+++ hello_world/forms/hello_world.js
@@ -0,0 +1,11 @@
+
+/**
+ * Perform the element default action.
+ *
+ * @param {JSEvent} event the event that triggered the action
+ *
+ * @properties={typeid:24,uuid:"9CA0C242-2540-4848-B30D-056DE3F415B1"}
+ */
+function onAction(event) {
+	// TODO Auto-generated method stub
+}
